93f70c0e1896f16baeec51c4682de1ab64980793
Stephan Soolsma <ssoolsma@gmail.com>
2022-09-07 19:59:32 +0200

    Destroyed and Reconned entities

---
+++ SandBox.Tasks/Data/Entities/DestroyedUnit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandBox.Tasks.Data.Entities
+{
+    [Table("ddcs_units_dead")]
+    public class DestroyedUnit
+    {
+        [Column("idDDCS_units_dead")]
+        public int Id { get; set; }
+
+        [Column("mission_id")]
+        public string? MissionId { get; set; }
+
+        [Column("sortie_id")]
+        public string? SortieId { get; set; }
+
+        [Column("target_unit_id")]
+        public string? UnitId { get; set; }
+
+    }
+}
---
+++ SandBox.Tasks/Data/Entities/ReconnedUnit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandBox.Tasks.Data.Entities
+{
+    [Table("ddcs_units_recced")]
+    public class ReconnedUnit
+    {
+        [Column("idDDCS_units_recced")]
+        public int Id { get; set; }
+
+        [Column("mission_id")]
+        public int MissionId { get; set; }
+
+        [Column("unitName")]
+        public int UnitId { get; set; }
+
+        [Column("type")]
+        public int Description { get; set; }
+
+        [Column("clientName")]
+        public int DiscoveredBy { get; set; }
+
+        [Column("lat")]
+        public long Latitude { get; set; }
+
+        [Column("lon")]
+        public long Longitude { get; set; }
+
+        [Column("alt")]
+        public long Altitude { get; set; }
+
+    }
+}
