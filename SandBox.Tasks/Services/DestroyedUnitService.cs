3f9f7be53e089f6453f2090328ae9ee7ec1b94ff
Stephan Soolsma <ssoolsma@gmail.com>
2022-09-07 19:59:42 +0200

    DestroyedUnit Service

---
+++ SandBox.Tasks/Services/DestroyedUnitService.cs
@@ -0,0 +1,30 @@
+﻿using SandBox.Tasks.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SandBox.Tasks.Data.Entities;
+using SandBox.Tasks.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SandBox.Tasks.Services
+{
+    public class DestroyedUnitService
+    {
+        IUnitOfWork _uow;
+        IRepository<DestroyedUnit>? _repository;
+
+        public DestroyedUnitService(DbContext context)
+        {
+            _uow = new UnitOfWork(context);
+            _repository = _uow.Repository<DestroyedUnit>();
+
+        }
+
+        public IEnumerable<DestroyedUnit>? Fetch(string missionId)
+        {
+            return _repository?.FindAllBy(x => x.MissionId == missionId);
+        }
+    }
+}
