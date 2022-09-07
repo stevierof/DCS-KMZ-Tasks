2de5cb9f55dbf370956b994d26ceb6c4c0f863da
Stephan Soolsma <ssoolsma@gmail.com>
2022-09-07 20:00:03 +0200

    Factory pattern

---
+++ SandBox.Tasks/Factory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SandBox.Tasks.Data;
+using SandBox.Tasks.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable CS8618
+namespace SandBox.Tasks
+{
+    public class Factory
+    {
+        DbContext _context;
+
+        private DestroyedUnitService _destroyedUnitService;
+
+
+        public DestroyedUnitService? DestroyedUnitService
+        {
+            get
+            {
+                if (_destroyedUnitService == null)
+                    _destroyedUnitService = new DestroyedUnitService(_context);
+                return _destroyedUnitService;
+            }
+        }
+
+
+        public Factory(SandBoxContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
